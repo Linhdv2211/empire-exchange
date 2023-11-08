2ee526b6854839ee317ee2b5197f3ae2385fbec1
LinhDV <linhdv2211@gmail.com>
2023-11-08 23:14:46 +0700

    update base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './view/main-view/main-view.component';
+
+const routes: Routes = [
+  { path: 'home-page', component: MainViewComponent },
+  { path: '',   redirectTo: '/home-page', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
